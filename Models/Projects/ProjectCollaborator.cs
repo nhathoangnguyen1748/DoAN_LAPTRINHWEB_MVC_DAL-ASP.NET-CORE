ce15c500f4ccef10c0eed373dae9ee5f3bafa751
nhathoangnguyen117627 <nhathoangnguyen1748@gmail.com>
2026-06-19 12:45:02 +0700

    fix giao diện: add edit button on canvas tables

---
+++ Models/Projects/ProjectCollaborator.cs
@@ -0,0 +1,8 @@
+﻿namespace DoAnLapTrinhWeb.Models.Projects;
+
+public class ProjectCollaborator
+{
+    public int Id { get; set; }
+    public string ProjectId { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
