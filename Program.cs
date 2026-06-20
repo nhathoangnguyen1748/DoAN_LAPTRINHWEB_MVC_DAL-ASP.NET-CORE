dac0b1f7a50db327e8e74b1a7a0698bd511d3cdf
Luong <luong1305.960@gmail.com>
2026-06-20 20:06:04 +0700

    Sua xoa file compose

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using DoAnLapTrinhWeb.Data;
+using DoAnLapTrinhWeb.Data;
 using DoAnLapTrinhWeb.Services;
 using DoAnLapTrinhWeb.Hubs;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -45,7 +46,16 @@ builder.Services.AddSingleton<AspNetMvcExportService>();
 builder.Services.AddSingleton<MockDataService>();
 builder.Services.AddScoped<ProjectStore>();
 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownIPNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 using (var scope = app.Services.CreateScope())
 {
